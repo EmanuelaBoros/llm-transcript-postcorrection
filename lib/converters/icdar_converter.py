f898109d346b7d57e1eb9d48f3a6231f6c9013ca
Emanuela Boros <emanuela.boros@gmail.com>
2023-04-14 11:20:07 +0200

    added sentence splitting alignment: OCR sentence <-> ground truth sentence

--- lib/converters/icdar_converter.py
+++ lib/converters/icdar_converter.py
@@ -6,6 +6,9 @@ from fastdtw import fastdtw
 from nltk.tokenize import sent_tokenize
 from nltk import word_tokenize
 from scipy.spatial.distance import euclidean
+from tqdm import tqdm
+from scipy.spatial.distance import cdist
+import textdistance
 
 
 def process_text(text):
@@ -17,24 +20,20 @@ def process_text(text):
 
     return sentences
 
+def custom_similarity(sentence1, sentence2):
+    return textdistance.jaccard(sentence1, sentence2)
 
-def align_sentences(sentences_ocr, sentences_gs):
-    """
-    :param sentences_ocr: 
-    :param sentences_gs:
-    :return:
-    """
+
+def align_sentences(sentences1, sentences2):
+
     aligned_sentences = []
-
-    # Tokenize the sentences into words
-    tokenized_ocr = [word_tokenize(s) for s in sentences_ocr]
-    tokenized_gs = [word_tokenize(s) for s in sentences_gs]
-
-    # Perform the alignment using DTW
-    _, path = fastdtw(tokenized_ocr, tokenized_gs, dist=euclidean)
-
-    for idx_ocr, idx_gs in path:
-        aligned_sentences.append((sentences_ocr[idx_ocr], sentences_gs[idx_gs]))
+    for sentence1 in sentences1:
+        best_match = (None, 0)
+        for sentence2 in sentences2:
+            similarity = custom_similarity(sentence1, sentence2)
+            if similarity > best_match[1]:
+                best_match = (sentence2, similarity)
+        aligned_sentences.append((sentence1, best_match[0]))
 
     return aligned_sentences
 
@@ -45,13 +44,16 @@ def process_file(input_file, output_file):
         data = infile.readlines()
 
     # Extract OCR and GS sentences from the data list
-    ocr_sentences = process_text(data[0])
-    gs_sentences = process_text(data[2])
+    # [OCR_toInput] [OCR_aligned] [ GS_aligned]
+    # import pdb;pdb.set_trace()
+    ocr_sentences = process_text(data[0].replace('[OCR_toInput]', '').strip())
+    gs_sentences = process_text(data[2].replace('[ GS_aligned]', '').strip())
 
     # Align the OCR and GS sentences
     aligned_sentences = align_sentences(ocr_sentences, gs_sentences)
 
     # Write the output to a JSON Lines file
+    # import pdb;pdb.set_trace()
     with open(output_file, "w") as outfile:
         for ocr_sentence, gs_sentence in aligned_sentences:
             json_line = json.dumps({"ocr_text": ocr_sentence, "correct_text": gs_sentence})
@@ -66,10 +68,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for root, dirs, files in os.walk(args.input_dir):
-        for file in files:
-            # Check if the file has the desired format (e.g., .txt)
-            if file.endswith(".txt"):
+        for file in tqdm(files, total=len(files)):
+            if file.endswith(".txt") and 'readme' not in file:
                 input_file = os.path.join(root, file)
-                output_file = os.path.join(args.output_dir, file.replace(".txt", ".jsonl"))
+                print('Analyzing file {}'.format(input_file))
+                # Compute the output file path by replacing the input directory with the output directory
+                output_file = os.path.join(args.output_dir,
+                                           os.path.relpath(input_file, args.input_dir)).replace(".txt", ".jsonl")
+                print('Writing output {}'.format(output_file))
+                # Create the output directory if it does not exist
+                output_dir_path = os.path.dirname(output_file)
+                if not os.path.exists(output_dir_path):
+                    os.makedirs(output_dir_path)
 
                 process_file(input_file, output_file)
