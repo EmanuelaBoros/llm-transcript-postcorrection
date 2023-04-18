dafb21d712182075b764b2ee554e5cce2ca37068
Emanuela Boros <emanuela.boros@gmail.com>
2023-04-18 16:12:39 +0200

    fast sentence splitting + logging

--- lib/converters/converter_nzz.py
+++ lib/converters/converter_nzz.py
@@ -2,6 +2,9 @@ import os
 from bs4 import BeautifulSoup
 import argparse
 from typing import Tuple
+from nltk.tokenize import sent_tokenize
+import textdistance
+import logging
 
 def extract_text_equiv(gt_path: str, pred_path: str, extraction_type: str) -> None:
     # Recursively search for XML files in the ground truth folder
@@ -45,22 +48,56 @@ def extract_text_equiv(gt_path: str, pred_path: str, extraction_type: str) -> No
                             gt_line_text = gt_line.find('TextEquiv').text
                             pred_line_text = pred_line.find('TextEquiv').text
 
-                            # Print the extracted TextEquiv content for the TextLine in the ground truth and prediction files
+                            # Print the extracted TextEquiv content for the TextLine in the ground truth and
+                            # prediction files
                             print(f'Ground truth TextLine {line_id}:\n{gt_line_text}\n')
                             print(f'Prediction TextLine {line_id}:\n{pred_line_text}\n')
 
                     # If the extraction type is "region", skip the TextLine iteration
                     elif extraction_type == 'region':
-                        pass
+                        gt_region_sentences = sent_tokenize(gt_region_text)
+                        pred_region_sentences = sent_tokenize(pred_region_text)
+                        for gt_sentence in gt_region_sentences:
+                            # Calculate Jaccard similarity between the ground truth sentence and all prediction
+                            # sentences
+                            similarities = [textdistance.jaccard(gt_sentence, pred_sentence) for pred_sentence in
+                                            pred_region_sentences]
+
+                            # Find the index of the highest similarity score
+                            best_match_index = similarities.index(max(similarities))
+
+                            # Retrieve the best matching sentence from the prediction sentences
+                            matched_sentence = pred_region_sentences[best_match_index]
+
+                            # Print the ground truth sentence and its corresponding predicted sentence
+                            print(f'Ground truth sentence:\n{gt_sentence}\n')
+                            print(f'Predicted sentence:\n{matched_sentence}\n')
+
 
                     # Raise an error if the extraction type is invalid
                     else:
                         raise ValueError(f'Invalid extraction type "{extraction_type}"')
 
-def main() -> None:
+
+if __name__ == "__main__":
     # Create an argument parser for the ground truth and prediction paths
     parser = argparse.ArgumentParser(description='Extract TextEquiv content for ground truth and prediction files.')
     parser.add_argument('gt_path', type=str, help='Path to ground truth folder')
     parser.add_argument('pred_path', type=str, help='Path to prediction folder')
     parser.add_argument('--extraction-type', type=str, default='region', choices=['region', 'line'],
                         help='Specify whether to extract the TextEquiv content per region or per line')
+    parser.add_argument(
+        "--debug",
+        help="Print lots of debugging statements",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    parser.add_argument(
+        "--verbose",
+        help="Be verbose",
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
+    )
