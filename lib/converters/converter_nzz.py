350eaf8a2e67348a5233ef284e1a115818b09f51
Emanuela Boros <emanuela.boros@gmail.com>
2023-04-20 08:56:27 +0200

    update converter [line/region/sentence]

--- lib/converters/converter_nzz.py
+++ lib/converters/converter_nzz.py
@@ -2,7 +2,7 @@ import os
 from bs4 import BeautifulSoup
 import argparse
 from typing import Tuple
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 import textdistance
 import logging
 from tqdm import tqdm
@@ -24,13 +24,48 @@ def align_sentences(sentences1, sentences2):
     for sentence1 in sentences1:
         best_match = (None, 0)
         for sentence2 in sentences2:
-
-
-
             similarity = custom_similarity(sentence1, sentence2)
             if similarity > best_match[1]:
+                print(similarity)
                 best_match = (sentence2, similarity)
         aligned_sentences.append((sentence1, best_match[0]))
+
+    return aligned_sentences
+
+
+def process_text(text):
+    cleaned_text = text.strip()
+    words = word_tokenize(cleaned_text)
+    return words
+
+
+def custom_similarity(word1, word2):
+    return textdistance.jaro_winkler(word1, word2)
+
+
+def align_texts(text1, text2):
+    words1 = process_text(text1)
+    words2 = process_text(text2)
+
+    aligned_words = textdistance.dtw(words1, words2, lambda w1, w2: 1 - custom_similarity(w1, w2))
+
+    aligned_sentences = []
+    sentence1 = []
+    sentence2 = []
+
+    for word1, word2 in aligned_words.path:
+        if word1 is not None:
+            sentence1.append(words1[word1])
+        if word2 is not None:
+            sentence2.append(words2[word2])
+
+        if word1 is not None and word2 is not None and (words1[word1] == '.' or words2[word2] == '.'):
+            aligned_sentences.append((' '.join(sentence1).strip(), ' '.join(sentence2).strip()))
+            sentence1 = []
+            sentence2 = []
+
+    if sentence1 or sentence2:
+        aligned_sentences.append((' '.join(sentence1).strip(), ' '.join(sentence2).strip()))
 
     return aligned_sentences
 
@@ -41,6 +76,7 @@ def process_file(input_file, ocr_file, output_file, extraction_type):
         with open(input_file, 'r') as f:
             gt_soup = BeautifulSoup(f, 'xml')
 
+        aligned_texts = []
         # Parse the prediction file
         if not os.path.exists(ocr_file):
             print(f'{ocr_file} does not exist.')
@@ -55,18 +91,18 @@ def process_file(input_file, ocr_file, output_file, extraction_type):
                 ocr_region = ocr_soup.find('TextRegion', {'id': region_id})
 
                 # Extract the TextEquiv content for the TextRegion in the ground truth and prediction files
-                gt_region_text = gt_region.find('TextEquiv').text.strip()
+                gt_region_text = gt_region.findAll('TextEquiv')[-1].text.strip()
 
                 ocr_region_text = None
                 try:
-                    ocr_region_text = ocr_region.find('TextEquiv').text.strip()
+                    ocr_region_text = ocr_region.findAll('TextEquiv')[-1].text.strip()
                 except:
                     print(f'{region_id} not found in {ocr_file}')
                 # Print the extracted TextEquiv content for the TextRegion in the ground truth and prediction files
 
                 # If the extraction type is "line", iterate over each TextLine in the TextRegion
                 if extraction_type == 'line':
-                    aligned_lines = []
+                    # aligned_lines = []
                     for gt_line in gt_region.find_all('TextLine'):
                         # Find the corresponding TextLine in the prediction file
                         line_id = gt_line['id']
@@ -81,32 +117,34 @@ def process_file(input_file, ocr_file, output_file, extraction_type):
                         print(f'Ground truth TextLine {line_id}:\n{gt_line_text}\n')
                         print(f'Prediction TextLine {line_id}:\n{pred_line_text}\n')
 
-                        aligned_lines.append((gt_line, ocr_line))
-                    # Write the output to a JSON Lines file
-                    with open(output_file, "w") as outfile:
-                        for ocr_line, gs_line in aligned_lines:
-                            json_line = json.dumps({"ocr_text": ocr_line, "correct_text": gs_line})
-                            outfile.write(json_line + "\n")
+                        aligned_texts.append((gt_line, ocr_line))
 
                 # If the extraction type is "region", skip the TextLine iteration
                 elif extraction_type == 'region':
+
+                    if ocr_region_text:
+                        # Align the OCR and GS sentences
+                        aligned_texts += [(gt_region_text, ocr_region_text)]
+
+                    # If the extraction type is "region", skip the TextLine iteration
+                elif extraction_type == 'sentence':
 
                     gt_region_sentences = sent_tokenize(gt_region_text)
                     if ocr_region_text:
                         pred_region_sentences = sent_tokenize(ocr_region_text)
 
                         # Align the OCR and GS sentences
-                        aligned_sentences = align_sentences(gt_region_sentences, pred_region_sentences)
-
-                        # Write the output to a JSON Lines file
-                        with open(output_file, "w") as outfile:
-                            for ocr_sentence, gs_sentence in aligned_sentences:
-                                json_line = json.dumps({"ocr_text": ocr_sentence, "correct_text": gs_sentence})
-                                outfile.write(json_line + "\n")
+                        aligned_texts += align_texts(gt_region_text, ocr_region_text)
 
                 # Raise an error if the extraction type is invalid
                 else:
                     raise ValueError(f'Invalid extraction type "{extraction_type}"')
+
+            # Write the output to a JSON Lines file
+            with open(output_file, "w") as outfile:
+                for ocr_text, gs_text in aligned_texts:
+                    json_line = json.dumps({"ocr_text": ocr_text, "correct_text": gs_text})
+                    outfile.write(json_line + "\n")
 
 
 if __name__ == "__main__":
@@ -114,7 +152,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract TextEquiv content for ground truth and prediction files.')
     parser.add_argument('--input_dir', type=str, help='Path to ground truth folder')
     parser.add_argument('--ocr_dir', type=str, help='Path to OCRed folder')
-    parser.add_argument('--extraction_type', type=str, default='region', choices=['region', 'line'],
+    parser.add_argument('--extraction_type', type=str, default='region', choices=['region', 'line', 'sentence'],
                         help='Specify whether to extract the TextEquiv content per region or per line')
     parser.add_argument("--output_dir", help="The path to the output directory where JSON Lines files will be created.")
     parser.add_argument(
